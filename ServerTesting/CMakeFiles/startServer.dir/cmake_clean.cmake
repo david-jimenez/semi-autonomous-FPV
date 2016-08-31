fc97fb64db93ad0244322d9ef732179549d4c9f5
Salty-Pete <robert.nash42@gmail.com>
2016-08-31 05:47:38 +0000

    did this work?

---
+++ ServerTesting/CMakeFiles/startServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/startServer.dir/server.c.o"
+  "startServer.pdb"
+  "startServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/startServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ clientTesting/CMakeFiles/startClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/startClient.dir/clientTest.c.o"
+  "startClient.pdb"
+  "startClient"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/startClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
